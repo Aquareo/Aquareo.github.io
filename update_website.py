640da63a1dd6925e965721b9a5249b11198f6af9
Liu yu <69413666+Aquareo@users.noreply.github.com>
2024-11-21 17:04:07 +0800

    Update update_website.py

--- update_website.py
+++ update_website.py
@@ -14,8 +14,12 @@ def load_ranking():
     # 添加 'rank' 列，使用 DataFrame 的 reset_index 来生成一个基于排序后的行号的新的索引
     sorted_df['rank'] = sorted_df.reset_index().index + 1  # rank从1开始
     
+    # 添加 'score' 列，第一个排名100分，之后依次递减
+    max_score = 100
+    sorted_df['score'] = max_score - (sorted_df['rank'] - 1) * 5  # 每降一名，减5分
+
     # 选择需要的列，并重命名
-    sorted_df = sorted_df[['rank', 'symbol', 'name']]
+    sorted_df = sorted_df[['rank', 'symbol', 'name', 'score']]
     
     # 重命名列
     ranking_df = sorted_df.rename(columns={'symbol': 'ticker'})
